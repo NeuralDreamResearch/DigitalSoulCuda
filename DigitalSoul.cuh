b61057ec308ca545e3dee4a875497c53c4e6ccc4
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2024-01-29 05:38:46 +0300

    Adding Polynomial Compute Unit into DigitalSoul.cuh

--- DigitalSoul.cuh
+++ DigitalSoul.cuh
@@ -28,7 +28,139 @@ namespace QN {
     namespace metadata{};
 }
 
+namespace metadata
+{
+	namespace VHDL
+	{
+		std::string fixed_point_arithmetic=R"(library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
+entity HalfAdder is Port(a,b:in std_logic; c_out, sum:out std_logic); end HalfAdder;
+
+architecture Behavioral of HalfAdder is
+begin
+    c_out<=a and b;
+    sum<=a xor b;
+end Behavioral;
+------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FullAdder is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           c_in : in STD_LOGIC;
+           c_out : out STD_LOGIC;
+           sum : out STD_LOGIC);
+end FullAdder;
+
+architecture Behavioral of FullAdder is
+
+begin
+    sum<=a xor b xor c_in;
+    c_out<=((a xor b) and c_in) or (a and b);
+end Behavioral;
+
+------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FixedPointAdder is -- a+b=c
+    generic(N:natural:=32);
+    port(a,b:in std_logic_vector(N-1 downto 0); c: out std_logic_vector(N-1 downto 0); overflow:out std_logic);
+end FixedPointAdder;
+
+architecture Behavioral of FixedPointAdder is
+    signal c_inter: std_logic_vector(N-1 downto 0);
+begin
+    FA1: entity work.FullAdder port map(a=>a(0), b=>b(0), c_in=>'0', c_out=>c_inter(0), sum=>c(0));
+    FA_array: for i in 1 to N-1 generate FA: entity work.FullAdder port map(a=>a(i), b=>b(i),c_in=>c_inter(i-1),c_out=>c_inter(i), sum=>c(i));
+    end generate FA_array;
+    overflow<=c_inter(N-1);
+
+end architecture Behavioral;
+-----------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FixedPointSubtractor is -- a - b
+    generic(N: natural:=32);
+    port(a,b:in std_logic_vector(N-1 downto 0); c: out std_logic_vector(N-1 downto 0); overflow: out  std_logic);
+end FixedPointSubtractor;
+
+architecture Behavioral of FixedPointSubtractor is
+    signal c_inter,b_inter: std_logic_vector(N-1 downto 0);
+begin
+    b_inter<=not(b);
+    FA1: entity work.FullAdder port map(a=>a(0), b=>b_inter(0),c_in=>'1',c_out=>c_inter(0), sum=>c(0));
+    FA_array: for i in 1 to N-1 generate FA: entity work.FullAdder port map(a=>a(i), b=>b_inter(i),c_in=>c_inter(i-1),c_out=>c_inter(i), sum=>c(i)); end generate FA_array;
+    overflow<=not c_inter(N-1);
+end Behavioral;
+----------------------------------------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FixedPointMultiplier_MixedPrecision is
+    generic(N:natural:=32);
+    port(a,b:in std_logic_vector(N-1 downto 0); c: out std_logic_vector(2*N-1 downto 0));
+end FixedPointMultiplier_MixedPrecision;
+
+architecture Behavioral of FixedPointMultiplier_MixedPrecision is
+    signal anded: std_logic_vector(N**2-1 downto 0);
+    signal sumout: std_logic_vector(N**2+N-1 downto 0);--N rows, N+1 cols
+    signal carries: std_logic_vector(N-1 downto 0);
+begin
+    and_array: for i in 0 to N-1 generate 
+        inner_loop: for j in 0 to N-1 generate
+            gated: anded(N*i+j)<=a(j) and b(i); 
+        end generate inner_loop; 
+    end generate and_array;
+    carries(0)<='0';
+    sumout(N downto 1)<=anded(N-1 downto 0);
+
+    add_array: for i in 1 to N-1 generate
+        adder: entity work.FixedPointAdder generic map(N=>N+1) 
+        port map(a=>carries(i-1)&sumout((N+1)*(i)-1 downto (N+1)*(i-1)+1),
+                                                                        b=>anded(N*(i+1)-1 downto N*i)&'0',
+                                                                        overflow=>carries(i),
+                                                                        c=>sumout((N+1)*(i+1)-1 downto (N+1)*i) );
+    end generate add_array;
+
+    outport1: for i in 0 to N-2 generate
+        outport: c(i)<=sumout((N+1)*(i)+1);
+    end generate outport1;
+
+    c(2*N-2 downto N-1)<=sumout(N**2+N-1 downto N**2);
+    c(2*N-1)<=carries(N-1);
+end Behavioral;
+----------------------------------------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FixedPointFMA is
+    generic(N:natural:=32);
+    port(mul1, mul2, add: in std_logic_vector(N-1 downto 0); fused: out std_logic_vector(N-1 downto 0); overflow: out std_logic);
+end FixedPointFMA;
+
+
+architecture Behavioral of FixedPointFMA is
+    signal distillate: std_logic_vector(2*N-1 downto 0);
+    signal of_mul, of_add:std_logic;
+begin
+
+
+    multiplier: entity work.FixedPointMultiplier_MixedPrecision generic map(N=>N) port map(a=>mul1, b=>mul2, c=>distillate);
+    process(distillate) begin
+    if distillate(2*N-1 downto N) = (N-1 downto 0 => '0') then 
+        of_mul<='0'; 
+    else of_mul <= '1'; 
+        end if;
+    end process;
+    adder: entity work.FixedPointAdder generic map(N=>N) port map(a=>distillate(N-1 downto 0), b=>add, c=>fused, overflow=>of_add);
+    
+    overflow<=of_mul or of_add;
+end architecture Behavioral;)";}
+};
 
 namespace kernels
 {
@@ -235,6 +367,298 @@ public:
 	}
 
 
+
+};
+
+template<typename s=size_t,typename T=float> class Polynomial
+{
+private:
+	s N=0, *d_N;
+	T* coefficients, *d_coefficients;
+	bool residency[4]={true, false, false, false},cached=false;//0 CPU-C++, 1 NVGPU-CUDA+C++, 2 FPGA-VHDL, 3 QNetwork-C++
+    T obuf,*d_obuf;
+public:
+	Polynomial(){};
+	//Polynomial(s N, bool NVGPU): N(N) {};
+	Polynomial(s N, T* coefficients, bool NVGPU)
+	{
+		this->N=N; 
+		this->coefficients=coefficients;
+		if(NVGPU)
+		{
+			residency[1]=true;
+
+			cudaMalloc(&d_N, sizeof(s));
+			cudaMalloc(&d_obuf, sizeof(T));
+			cudaMalloc(&(this->d_coefficients), (this->N)*sizeof(T));
+
+			this->N-=1;
+			cudaMemcpy(this->d_N,&(this->N),sizeof(s),cudaMemcpyHostToDevice);
+			this->N+=1;
+			cudaMemcpy(this->d_coefficients, this->coefficients, N*sizeof(T), cudaMemcpyHostToDevice);
+			T *hh=new T[this->N];
+			cudaMemcpy(hh,d_coefficients,this->N*sizeof(float),cudaMemcpyDeviceToHost );
+		}
+		
+	}
+
+    void setN(s N)
+	{
+		cached=false;
+		this->N=N;
+		if(residency[1])
+		{
+			cudaMemcpy(this->d_N,&(this->N),sizeof(s),cudaMemcpyHostToDevice);
+		}
+		
+	}
+    s getN(void) {return N;};
+   
+    void setCoefficients(T* coefficients)
+	{
+		cached=false;
+		this->coefficients=coefficients;
+		if(residency[1])
+		{
+			cudaMemcpy(this->d_coefficients, this->coefficients, sizeof(T)*(this->N), cudaMemcpyHostToDevice);
+		}
+	}
+    T *getCoefficients(void) {return coefficients;}
+
+
+    // Compute utils
+	void computeCPU(T *ibuf, T *obuf)
+	{
+		
+		T tmp=1;
+		for(s i=N-1; i>0; i--)
+		{
+			(*obuf)+=tmp*coefficients[i];
+			tmp*=*ibuf;
+		}
+		
+		*obuf+=tmp*(*coefficients);
+	}
+
+	void computeGPU(T *ibuf, T *obuf)
+	{
+			
+		T *ee, *hee;
+		cudaMalloc(&ee, N*sizeof(T));
+
+		kernels::poly<T,s><<<1,N>>>(d_coefficients,*ibuf,ee,d_N);
+		hee=new T[N];
+		cudaDeviceSynchronize();
+		cudaMemcpy(hee,ee, N*sizeof(T),cudaMemcpyDeviceToHost);
+		for(size_t i=0;i<N;i++)
+		{
+			(*obuf)+=hee[i];
+		}
+	}
+
+
+	// Transpile
+	std::string entityGen(std::string name="")
+	{
+		
+		std::string base=R"(library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity CombinationalPolynomialCompute_FixedPoint_)"+name+R"( is
+	generic(bit_width:natural:=)"+std::to_string(8*sizeof(T))+R"(; order:natural:=)"+std::to_string(N-1)+R"();
+	Port(x:in std_logic_vector(bit_width-1 downto 0); y:out std_logic_vector(bit_width-1 downto 0); overflow: out std_logic);
+end CombinationalPolynomialCompute_FixedPoint_)"+name+R"(;
+
+architecture Behavioral of CombinationalPolynomialCompute_FixedPoint_)"+name+R"( is
+	signal pre_multiplied, pre_fma, coeff: std_logic_vector(bit_width*(order+1)-1 downto 0);
+	signal upstreamed:  std_logic_vector(2*bit_width*(order)-1 downto 0);
+	signal fma_of: std_logic_vector(order-1 downto 0);
+
+begin)";
+		std::string aa;
+		
+		for(size_t i=0;i<N;i++)
+		{
+			aa=std::string(8*sizeof(T), '0');
+			for(size_t j=0;j<8*sizeof(T);j++)
+			{
+				if(coefficients[N-1-i]&(1<<j)){aa[8*sizeof(T)-j-1]='1';}
+				
+			}
+			base+="\n\tcoeff("+std::to_string((i+1)*8*sizeof(T)-1)+" downto "+std::to_string(i*8*sizeof(T))+")<=\""+aa+'"'+';';
+			
+		}
+		base+=R"(
+pre_fma(bit_width-1 downto 0)<=(bit_width-1 downto 0 => '0');
+pre_multiplied(bit_width-1 downto 0)<=(bit_width-2 downto 0 => '0')&'1';
+	multiply_next: for i in 1 to order generate
+		aa:entity work.FixedPointMultiplier_MixedPrecision 
+			generic map(N=>bit_width)
+			port map(a=>x,
+					b=>pre_multiplied((i)*bit_width-1 downto (i-1)*bit_width),
+					c=>upstreamed(2*bit_width*(i)-1 downto 2*bit_width*(i-1)));
+		pre_multiplied((i+1)*bit_width-1 downto (i)*bit_width)<=upstreamed(2*bit_width*(i)-1-bit_width downto 2*bit_width*(i-1));
+	end generate multiply_next; 
+
+fma_array: for i in 1 to order generate
+	fma: entity work.FixedPointFMA generic map(N=>bit_width) 
+	   port map(overflow=>fma_of(i-1),
+	            add=>pre_fma(i*bit_width-1 downto (i-1)*bit_width),
+	            mul1=>pre_multiplied((i)*bit_width-1 downto (i-1)*bit_width),
+	            mul2=>coeff(i*bit_width-1 downto (i-1)*bit_width),
+	            fused=>pre_fma((i+1)*bit_width-1 downto (i)*bit_width));
+end generate fma_array;
+
+process(fma_of) begin
+    if fma_of/=(order-1 downto 0 => '0') then overflow<='1'; else overflow<='0'; end if;
+end process;
+
+y<=pre_fma(bit_width*(order+1)-1 downto bit_width*order);
+	
+end Behavioral;
+	)";
+	return base;
+	}
+
+	// Human Interface
+	std::string ss()
+	{
+		std::string a=std::to_string(N-1);
+		switch((N-1)%10)
+		{
+			case 1:
+				a+="st"; break;
+			case 2:
+				a+="nd"; break;
+			case 3:
+				a+="rd"; break;
+			default:
+				a+="th";
+		}
+
+		a+=" order Polynomial P(x)=";
+		for(s i=N-1;i>0;i--)
+		{
+			a+=std::to_string(*(coefficients+this->N-i-1))+'*'+'x'+'^'+std::to_string(i)+'+';
+		}
+        a+=std::to_string(coefficients[N-1]);
+
+		return a;
+	};
+
+    Polynomial operator+ (Polynomial<s,T>& operand)
+    {
+        bool i_am_max_n=(this->N >= operand.N)?true:false;
+        s maxn;
+        if (i_am_max_n)
+        {
+            maxn=this->N;
+            T *new_coeff=new T[this->N];
+            for(s i=0;i<operand.N;i++)
+            {
+                *(new_coeff+i)=*(this->coefficients + i) + *(operand.coefficients + i);
+            }
+            for(s i=operand.N;i<this->N;i++)
+            {
+                *(new_coeff+i)=*(this->coefficients+i);
+            }
+            return Polynomial<s,T>(maxn, new_coeff, false);
+        }
+        else
+        {
+            T *new_coeff=new T[operand.N];
+            maxn=operand.N;
+            for(s i=0;i<this->N;i++)
+            {
+                *(new_coeff+i)=*(this->coefficients + i) + *(operand.coefficients + i);
+            }
+            for(s i=this->N;i<operand.N;i++)
+            {
+                *(new_coeff+i)=*(operand.coefficients+i);
+            }
+            return Polynomial<s,T>(maxn, new_coeff, false);
+        }   
+    };
+    
+    Polynomial operator- (Polynomial<s,T>& operand)
+    {
+        bool i_am_max_n=(this->N >= operand.N)?true:false;
+        s maxn;
+        if (i_am_max_n)
+        {
+            maxn=this->N;
+            T *new_coeff=new T[this->N];
+            for(s i=0;i<operand.N;i++)
+            {
+                *(new_coeff+i)=*(this->coefficients + i) - *(operand.coefficients + i);
+            }
+            for(s i=operand.N;i<this->N;i++)
+            {
+                *(new_coeff+i)=*(this->coefficients+i);
+            }
+            return Polynomial<s,T>(maxn, new_coeff, false);
+        }
+        else
+        {
+            T *new_coeff=new T[operand.N];
+            maxn=operand.N;
+            for(s i=0;i<this->N;i++)
+            {
+                *(new_coeff+i)=*(this->coefficients + i) - *(operand.coefficients + i);
+            }
+            for(s i=this->N;i<operand.N;i++)
+            {
+                *(new_coeff+i)=-*(operand.coefficients+i);
+            }
+            return Polynomial<s,T>(maxn, new_coeff, false);
+        }   
+    };
+
+    Polynomial<s,T> operator*(Polynomial<s,T>& operand)
+    {
+        s maxn=(this->N)+(operand.N)-1;
+        T *new_coeff=new T[maxn];
+        for(s i=0; i<this->N;i++)
+        {
+            for(s j=0; j<operand.N;j++)
+            {
+
+                new_coeff[i+j]+=(this->coefficients[i])*(operand.coefficients[j]);
+                
+            }
+            
+        }
+
+        return Polynomial<s,T>(maxn, new_coeff,false);
+    }
+
+    Polynomial<s,T> operator/(Polynomial<s,T>& operand)
+    {
+        s maxn=(this->N)-(operand.N)+1;
+        T *new_coeff=new T[maxn];
+        T *tmp_coeff=new T[this->N];
+
+        for(size_t k=0;k<this->N;k++)
+        {
+            tmp_coeff[k]=this->coefficients[k];
+
+        }
+
+        for(size_t i=0;i<maxn;i++)
+        {
+            
+            T mul=(tmp_coeff[i])/(*(operand.coefficients));
+            new_coeff[i]=mul;
+            for(size_t j=0;j<operand.N;j++)
+            {
+                tmp_coeff[i+j]-=mul*operand.coefficients[j];
+            }
+
+            
+        }    
+        return Polynomial(maxn,new_coeff, false);
+
+        }
 
 };
 
